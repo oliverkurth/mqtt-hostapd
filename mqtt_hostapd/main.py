1f26d5333a218a794d1a576eb8f3474fc43d0cf3
Oliver Kurth <okurth@gmail.com>
2023-01-30 17:59:56 -0800

    autodiscovery and other improvements

--- mqtt_hostapd/main.py
+++ mqtt_hostapd/main.py
@@ -1,18 +1,18 @@
+import json
+import os
 import paho.mqtt.client as mqtt
 import socket
-import os
+import yaml
 
-# HTTP settings
-PORT = 8000
-USE_HTTPS = False
+CONFIG_FILE = "config.yaml"
 
 # MQTT settings
 MQTT_SERVER="192.168.2.22"
 MQTT_PORT=1883
+MQTT_STATE_TOPIC = "state/hostapd"
+MQTT_ID_PREFIX = "hostapd-"
 
-TOPIC="hostapd"
-
-HOSTAPD_CTRL = "/var/run/hostapd/ap1"
+HOSTAPD_CTRL_DIR = "/var/run/hostapd"
 
 
 def on_connect(client, userdata, flags, rc):
@@ -26,12 +26,41 @@ def on_disconnect(client, userdata, rc):
         print ("unexpected MQTT disconnect\n")
 
 
-def connect_hostapd():
+def connect_mqtt(mqtt_config):
+    client = mqtt.Client()
+    client.is_connected = False
+    client.connect(mqtt_config.get('server', MQTT_SERVER), mqtt_config.get('port', MQTT_PORT), 60)
+    client.on_connect = on_connect
+    client.on_disconnect = on_disconnect
+    return client
+
+
+def mqtt_autodiscovery(client, mqtt_config, devices):
+    if not mqtt_config.get('autodiscovery', False):
+        return
+
+    for id, device in devices.items():
+        if not device.get('autodiscovery', True):
+            continue
+        data = {
+            "state_topic" : "{}/{}".format(mqtt_config.get('state_topic', MQTT_STATE_TOPIC), id),
+            "name" : device.get('name', id),
+            "unique_id" : "{}{}".format(mqtt_config.get('id_prefix', MQTT_ID_PREFIX), id),
+            "payload_home" : "connected",
+            "payload_not_home" : "disconnected",
+            "source_type" : "router"
+        }
+        topic = "homeassistant/device_tracker/{}/config".format(id)
+
+        client.publish(topic, json.dumps(data))
+
+
+def connect_hostapd(hostapd_config):
     s = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
     s.settimeout(10)
     s_local = "/tmp/mqtt-hostapd-{}".format(os.getpid())
     s.bind(s_local)
-    s.connect(HOSTAPD_CTRL)
+    s.connect(os.path.join(hostapd_config.get("ctrl_dir", HOSTAPD_CTRL_DIR), hostapd_config.get("interface")))
     s.send("ATTACH")
 
     response = s.recv(1024)
@@ -42,38 +71,46 @@ def connect_hostapd():
 
 
 def parse_hostapd_data(data):
-    what,mac = data[data.find(">") + 1:].split(' ')
-    return what, mac
+    try:
+        what, mac = data[data.find(">") + 1:].split(' ')
+        id = mac.replace(':', '')
+        return what, id
+    except ValueError:
+        print ("could not parse '{}'".format(data))
+        return None, None
 
 
 def main():
-    client = mqtt.Client()
-    client.is_connected = False
-    client.connect(MQTT_SERVER, MQTT_PORT, 60)
-    client.on_connect = on_connect
-    client.on_disconnect = on_disconnect
+    with open(CONFIG_FILE, 'r') as f:
+        config = yaml.safe_load(f)
 
-    s = connect_hostapd()
+    client = connect_mqtt(config['mqtt'])
+    mqtt_autodiscovery(client, config['mqtt'], config['devices'])
+
+    s = connect_hostapd(config["hostapd"])
 
     while True:
         client.loop()
 
         try:
             data = s.recv(1024)
-            what, mac = parse_hostapd_data(data)
+            what, id = parse_hostapd_data(data)
+
             if what == "AP-STA-CONNECTED":
                 what = "connected"
             elif what == "AP-STA-DISCONNECTED":
                 what = "disconnected"
             else:
                 what = "unknown"
-            print ("{} {}".format(what, mac))
+            print ("{} {}".format(what, id))
 
-            if not client.is_connected:
-                print ("MQTT reconnect\n")
-                client.reconnect()
+            if what == "connected" or what == "disconnected":
+                if not client.is_connected:
+                    print ("MQTT reconnect\n")
+                    client.reconnect()
 
-            client.publish("stat/{}/{}".format(TOPIC, mac), what)
+                client.publish("{}/{}".format(config['mqtt'].get('state_topic', MQTT_STATE_TOPIC), id), what)
+
         except socket.timeout:
             pass
 
